4b806be490f2eeb8463f489e532c9345f7e3a43e
Chris Larsen <clarsendev@protonmail.com>
2020-04-03 01:12:17 -0400

    Add global report

--- test/covid_report_test.cr
+++ test/covid_report_test.cr
@@ -5,7 +5,7 @@ require "/../src/daily_news/covid_report.cr"
 class CovidReportTest < Minitest::Test
   def initialize(args)
     super(args)
-    @report = <<-REPORT
+    @canada_report = <<-REPORT
     COVID19 Report for Canada 2020-03-24
     Total Cases:      2790
     New Cases:        702
@@ -15,9 +15,24 @@ class CovidReportTest < Minitest::Test
     Mortality Rate:   0.9%
     Curve:            Increasing
     REPORT
+
+    @global_report = <<-REPORT
+    COVID19 Global Report for 2020-03-24
+    Total Cases:      418678
+    New Cases:        40131
+    Total Recovered:  0
+    Total Deaths:     18625
+    Infection Rate:   0.005%
+    Mortality Rate:   4.4%
+    Curve:            Steady
+    REPORT
   end
 
-  def test_prints_report
-    assert_equal @report, DailyNews.covid_report("CAN")
+  def test_prints_country_report
+    assert_equal @canada_report, DailyNews.covid_report("CAN")
+  end
+
+  def test_prints_global_report
+    assert_equal @global_report, DailyNews.global_covid_report
   end
 end
